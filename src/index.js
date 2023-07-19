1b704bb8e6151a5bd128873d68c6f923d1bcb948
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-18 21:49:24 -0300

    handle route not found

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.get("/accounts", accountsController.index);
 app.post("/accounts", accountsController.store);
 app.put("/accounts/:id", accountsController.update);
 
+app.use((req, res, next) =>
+  res.status(404).json({ message: "route not found" })
+);
+
 app.use((err, req, res, next) => {
   console.error(err);
   if (err instanceof Error) res.status(500).json({ message: err.message });
